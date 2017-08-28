a0c3a0b3a3caf77331ecbd99b8da4174d7be0d60
Lucient Lau <pursueexcel@gmail.com>
2017-08-28 20:47:50 +0800

    Modify quotes

--- scrapydemoz/scrapydemoz/spiders/quotes_spider.py
+++ scrapydemoz/scrapydemoz/spiders/quotes_spider.py
@@ -1,18 +1,17 @@
 import scrapy
 
 
-import scrapy
-
-
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
+        #'http://quotes.toscrape.com/page/2/',
     ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'hello_quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+         for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
